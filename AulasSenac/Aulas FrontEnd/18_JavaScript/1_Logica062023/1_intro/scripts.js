a805acf85d6601a763ee692a2ddbc6a98de8ed92
mathzinxss <math983839179@gmail.com>
2024-09-04 13:42:15 -0300

    14

---
+++ AulasSenac/Aulas FrontEnd/18_JavaScript/1_Logica062023/1_intro/scripts.js
@@ -0,0 +1,15 @@
+function primeira(num1,num2){
+    var resposta = num1 + num2;
+    return resposta;
+}
+
+document.getElementById('um').onclick = function(){
+    alert('Aiiii');
+}
+
+document.getElementById('dois').onclick =function(){
+    alert('oi');
+    //document.write('ui');
+    console.log('ai');
+    alert(45 * 38);
+}
