3639191a34a0bc7112a25d6a8ecbe78789beabd0
mathznxs.dev <math983839179@gmail.com>
2024-10-17 14:50:27 -0300

    2

--- AulasSenac/Aulas FrontEnd/18_JavaScript/8_nodeJS/1_introNode/1_index.js
+++
@@ -1,9 +0,0 @@
-let preco = 1000;
-if(preco >= 1000){
-    valFinal = preco*0.98;
-}else if(preco >= 800){
-    valFinal = preco*0.95;
-}else if(preco >= 500){
-    valFinal = preco*0.80;
-}
-console.log("O valor final é : " + valFinal);
