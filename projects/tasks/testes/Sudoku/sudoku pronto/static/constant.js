59063bcaf4452e7a6ee381e7ea0d9993bbf455ff
mathznxs.dev <math983839179@gmail.com>
2024-10-28 17:00:26 -0300

    1

--- projects/tasks/testes/Sudoku/sudoku pronto/static/constant.js
+++ projects/tasks/testes/Sudoku/sudoku pronto/static/constant.js
@@ -4,12 +4,12 @@ const CONSTANT = {
     BOX_SIZE: 3,
     NUMBERS: [1,2,3,4,5,6,7,8,9],
     LEVEL_NAME: [
-        'Easy',
-        'Medium',
-        'Hard',
-        'Very hard',
-        'Insane',
-        'Inhuman'
+        'Facil',
+        'Médio',
+        'Difícil',
+        'Muito Difícil',
+        'Insano',
+        'Inumano'
     ],
     LEVEL: [29, 38, 47, 56, 65, 74]
 }
